c28c62ee4c58f3c23f25e0d5aea351858cefe6c0
della-padula <della.kimko@gmail.com>
2019-11-10 14:17:43 +0900

    정보통계보험수리학과 리스트 완료

--- SoongsilNotice/NoticeNaturalScience.swift
+++ SoongsilNotice/NoticeNaturalScience.swift
@@ -200,7 +200,65 @@ class NoticeNaturalScience {
     }
     
     static func parseListActuarial(page: Int, completion: @escaping ([Notice]) -> Void) {
+        let noticeUrl = "\(NoticeURL.naturalScienceActuarialURL)\(page)"
+        var noticeList = [Notice]()
+        var authorList = [String]()
+        var titleList  = [String]()
+        var urlList = [String]()
+        var dateStringList = [String]()
+        var index = 0
         
+        Alamofire.request(noticeUrl).responseString(encoding: .utf8) { response in
+            switch(response.result) {
+            case .success(_):
+                if let data = response.result.value {
+                    do {
+                        let doc = try HTML(html: data, encoding: .utf8)
+                        for product in doc.css("div[class='tbl_head01 tbl_wrap'] td") {
+                            let content = product.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+                            print(content)
+                            switch (index % 5) {
+                            case 0: break
+                            case 1:
+                                // Title
+                                titleList.append(content)
+                                break
+                            case 2:
+                                // Author
+                                authorList.append(content)
+                                break
+                            case 3:
+                                // Date
+                                dateStringList.append(content)
+                                break
+                            case 4: break
+                            default: break
+                            }
+                            index += 1
+                        }
+                        
+                        for product in doc.css("div[class='tbl_head01 tbl_wrap'] td a") {
+                            print(product["href"] ?? "")
+                            urlList.append(product["href"] ?? "")
+                        }
+                    } catch let error {
+                        print("Error : \(error)")
+                    }
+                    
+                    index = 0
+                    for _ in urlList {
+                        let noticeItem = Notice(author: "", title: titleList[index], url: urlList[index], date: dateStringList[index])
+                        noticeList.append(noticeItem)
+                        index += 1
+                    }
+                    
+                    completion(noticeList)
+                }
+            case .failure(_):
+                print("Error message:\(String(describing: response.result.error))")
+                break
+            }
+        }
     }
     
     static func parseListBiomedical(page: Int, completion: @escaping ([Notice]) -> Void) {

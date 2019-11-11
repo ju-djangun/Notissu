057c0b12bcc3aa7d462ed59f1545c97c92354ca1
della-padula <della.kimko@gmail.com>
2019-11-12 08:43:45 +0900

    언론홍보학과 리스트 추가완료

--- SoongsilNotice/NoticeParser/NoticeSocial.swift
+++ SoongsilNotice/NoticeParser/NoticeSocial.swift
@@ -213,7 +213,99 @@ class NoticeSocial {
     }
     
     static func parseListJournalism(page: Int, completion: @escaping ([Notice]) -> Void) {
+        let noticeUrl = "\(NoticeURL.socialJournalismURL)\(page)"
+        var noticeList = [Notice]()
+        var authorList = [String]()
+        var titleList  = [String]()
+        var urlList = [String]()
+        var dateStringList = [String]()
+        var index = 0
         
+        print("\(noticeUrl) : \(page)")
+        
+        Alamofire.request(noticeUrl).responseString(encoding: .utf8) { response in
+            switch(response.result) {
+            case .success(_):
+                if let data = response.result.value {
+                    do {
+                        print("after request")
+                        let doc = try HTML(html: data, encoding: .utf8)
+                        index = 0
+                        
+                        let product = doc.css("table[class='bbs-list']").first!
+                        
+                        if page > 1 {
+                            for item in product.css("tbody tr:not(.trNotice) td") {
+                                //print("***")
+                                let content = item.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+                                print(content)
+                                switch (index % 5) {
+                                case 0:
+                                    // Title
+                                    titleList.append(content)
+                                    break
+                                case 1: break
+                                case 2:
+                                    // Author
+                                    authorList.append(content)
+                                    break
+                                case 3:
+                                    // Date
+                                    dateStringList.append(content)
+                                    break
+                                case 4: break
+                                default: break
+                                }
+                                index += 1
+                            }
+                        } else {
+                        for item in product.css("tbody tr td") {
+                            //print("***")
+                            let content = item.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+                            print(content)
+                            switch (index % 5) {
+                            case 0:
+                                // Title
+                                titleList.append(content)
+                                break
+                            case 1: break
+                            case 2:
+                                // Author
+                                authorList.append(content)
+                                break
+                            case 3:
+                                // Date
+                                dateStringList.append(content)
+                                break
+                            case 4: break
+                            default: break
+                            }
+                            index += 1
+                        }
+                        }
+                        
+                        for product in doc.css("td[class='left'] a") {
+                            print("http://pubad.ssu.ac.kr\(product["href"] ?? "")")
+                            urlList.append("http://pubad.ssu.ac.kr\(product["href"] ?? "")")
+                        }
+                    } catch let error {
+                        print("Error : \(error)")
+                    }
+                    
+                    index = 0
+                    for _ in urlList {
+                        let noticeItem = Notice(author: authorList[index], title: titleList[index], url: urlList[index], date: dateStringList[index])
+                        noticeList.append(noticeItem)
+                        index += 1
+                    }
+                    
+                    completion(noticeList)
+                }
+            case .failure(_):
+                print("Error message:\(String(describing: response.result.error))")
+                break
+            }
+        }
     }
     
     static func parseListLifeLong(page: Int, completion: @escaping ([Notice]) -> Void) {

df83d3fbb44c508902f6f20878994d9936960563
della-padula <della.kimko@gmail.com>
2019-11-12 00:42:13 +0900

    금융학부 리스트 추가

--- SoongsilNotice/NoticeParser/NoticeBusiness.swift
+++ SoongsilNotice/NoticeParser/NoticeBusiness.swift
@@ -214,6 +214,66 @@ class NoticeBusiness {
     }
     
     static func parseListFinance(page: Int, completion: @escaping ([Notice]) -> Void) {
+        let noticeUrl = "\(NoticeURL.businessFinanceURL)\(page)"
+        var noticeList = [Notice]()
+        var authorList = [String]()
+        var titleList  = [String]()
+        var urlList = [String]()
+        var dateStringList = [String]()
+        var index = 0
         
+        Alamofire.request(noticeUrl).responseString(encoding: .utf8) { response in
+            switch(response.result) {
+            case .success(_):
+                if let data = response.result.value {
+                    do {
+                        let doc = try HTML(html: data, encoding: .utf8)
+                        for product in doc.css("table[class='bbs-list'] td") {
+                            //print("***")
+                            let content = product.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+                            print(content)
+                            switch (index % 5) {
+                            case 0: break
+                            case 1:
+                                // Title
+                                titleList.append(content)
+                                break
+                            case 2:
+                                // Author
+                                authorList.append("")
+                                break
+                            case 3:
+                                // Date
+                                dateStringList.append(content)
+                                break
+                            case 4: break
+                            case 5: break
+                            default: break
+                            }
+                            index += 1
+                        }
+                        
+                        for product in doc.css("table[class='bbs-list'] a") {
+                            print(product["href"] ?? "")
+                            urlList.append(product["href"] ?? "")
+                        }
+                    } catch let error {
+                        print("Error : \(error)")
+                    }
+                    
+                    index = 0
+                    for _ in urlList {
+                        let noticeItem = Notice(author: authorList[index], title: titleList[index], url: urlList[index], date: dateStringList[index])
+                        noticeList.append(noticeItem)
+                        index += 1
+                    }
+                    
+                    completion(noticeList)
+                }
+            case .failure(_):
+                print("Error message:\(String(describing: response.result.error))")
+                break
+            }
+        }
     }
 }

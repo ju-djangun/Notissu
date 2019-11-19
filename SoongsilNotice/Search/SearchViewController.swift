2f2aa9afb9c4ab49157d9a173e1a9b4678f2bfd3
della-padula <della.kimko@gmail.com>
2019-11-19 15:03:57 +0900

    소프트 공지 비공지 구분 완료

---
+++ SoongsilNotice/Search/SearchViewController.swift
@@ -0,0 +1,90 @@
+//
+//  SearchViewController.swift
+//  SoongsilNotice
+//
+//  Created by TaeinKim on 2019/11/19.
+//  Copyright © 2019 TaeinKim. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: BaseViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet var majorText       : UILabel!
+    @IBOutlet var pickerView      : UIPickerView!
+    @IBOutlet var searchBtn       : UIButton!
+    @IBOutlet var keywordTextField: UITextField!
+    
+    private var majorList = [DeptName]()
+    private var majorCodeList = [DeptCode]()
+    
+    private var selectedIndex = 0
+    private var selectedMajor = DeptName.IT_Computer
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for deptName in DeptName.allCases {
+            majorList.append(deptName)
+        }
+        
+        for deptCode in DeptCode.allCases {
+            majorCodeList.append(deptCode)
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func searchAction(_ sender: Any?) {
+        print("검색 : \(String(describing: self.keywordTextField.text))")
+    }
+    
+    @IBAction func selectMajorAction(_ sender: Any?) {
+        showPickerActionSheet()
+    }
+    
+    func showPickerActionSheet() {
+        let alert = UIAlertController(title: "전공을 선택하세요.", message: "\n\n\n\n\n\n", preferredStyle: .actionSheet)
+        alert.isModalInPopover = true
+        
+        let width = alert.view.frame.width
+        
+        print(alert.view.frame.width)
+        
+        let pickerFrame = UIPickerView(frame: CGRect(x: 0, y: 40, width: width - 16, height: 150))
+        
+        alert.view.addSubview(pickerFrame)
+        pickerFrame.dataSource = self
+        pickerFrame.delegate = self
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (UIAlertAction) in
+            
+            print("You selected " + self.selectedMajor.rawValue )
+            self.majorText.text = "선택한 전공 : \(self.selectedMajor.rawValue)"
+            
+        }))
+        self.present(alert,animated: true, completion: nil )
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.majorList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.majorList[row].rawValue
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedIndex = row
+        selectedMajor = self.majorList[row]
+    }
+    
+}

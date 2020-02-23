d3d7d1287009e694314914c9be4463e946233577
della-padula <della.kimko@gmail.com>
2020-02-23 13:14:46 +0900

    [NSI-908][NSI-017] Bookmark Toggle Image, Search VC Invalid Argument

---
+++ SoongsilNotice/Resources/NotissuImage.swift
@@ -0,0 +1,15 @@
+//
+//  NotissuImage.swift
+//  SoongsilNotice
+//
+//  Created by TaeinKim on 2020/02/23.
+//  Copyright © 2020 TaeinKim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct NotissuImage {
+    static let favoriteNavigationImageON  = UIImage(named: "notissu_favorite_on")!
+    static let favoriteNavigationImageOFF = UIImage(named: "notissu_favorite_off")!
+}

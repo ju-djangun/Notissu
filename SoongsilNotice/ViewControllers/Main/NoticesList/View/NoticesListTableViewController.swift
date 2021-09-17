96c82973380bc6e238fd2b38fc109e88ef325413
Gyuni <gyuni@soongsil.ac.kr>
2021-09-17 17:19:45 +0900

    [NSI-135] NoticesListTableViewController 분리

---
+++ SoongsilNotice/ViewControllers/Main/NoticesList/View/NoticesListTableViewController.swift
@@ -0,0 +1,120 @@
+//
+//  NoticesListTableViewController.swift
+//  SoongsilNotice
+//
+//  Created by Gyuni on 2021/09/17.
+//  Copyright © 2021 Notissu. All rights reserved.
+//
+
+import UIKit
+
+class NoticesListTableViewController: YDSTableViewController {
+    
+    private var viewModel: NoticesListViewModel
+    private var didReachedBottom: Bool {
+        self.tableView.contentOffset.y >
+            self.tableView.contentSize.height
+            - self.tableView.bounds.size.height
+            - Dimension.bottomRefreshHeight
+    }
+    private enum Dimension {
+        static let bottomRefreshHeight: CGFloat = 100
+    }
+    
+    var progressBarDelegate: ProgressBarDelegate?
+    
+    func setInitialData() {
+        viewModel.loadInitialPage()
+        progressBarDelegate?.showProgressBar()
+    }
+
+    init(with viewModel: NoticesListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindViewModel()
+        setupViews()
+    }
+    
+    private func bindViewModel() {
+        viewModel.noticesList.bind { [weak self] _ in
+            guard let `self` = self else { return }
+            self.tableView.reloadData()
+        }
+        
+        viewModel.nowLoading.bind { [weak self] value in
+            guard let `self` = self else { return }
+            if self.refreshControl?.isRefreshing ?? false && !value {
+                //  refresh control이 refresh 중인데
+                //  새로 내려온 nowLoading 값이 false인 경우에
+                //  refresch control이 refresh를 중단
+                self.refreshControl?.endRefreshing()
+            }
+            self.progressBarDelegate?.hideProgressBar()
+        }
+    }
+    
+    private func setupViews() {
+        setViewProperties()
+    }
+    
+    private func setViewProperties() {
+        tableView.register(NoticesListItemCell.self, forCellReuseIdentifier: "NoticesListItemCell")
+        tableView.separatorStyle = .none
+        tableView.refreshControl = UIRefreshControl()
+        tableView.refreshControl?.addTarget(self,
+                                 action: #selector(refreshData(sender:)),
+                                 for: .valueChanged)
+    }
+
+}
+
+//  MARK: - Data Source
+extension NoticesListTableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.noticesList.value.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: NoticesListItemCell =
+            tableView.dequeueReusableCell(withIdentifier: "NoticesListItemCell",
+                                          for: indexPath) as? NoticesListItemCell
+            ?? NoticesListItemCell()
+        let notice = viewModel.noticesList.value[indexPath.row]
+        cell.fillData(with: NoticesListItemViewModel(notice: notice))
+        return cell
+    }
+}
+
+//  MARK: - Delegate
+extension NoticesListTableViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let viewModel = NewNoticeDetailViewModel(notice: viewModel.noticesList.value[indexPath.row],
+                                                 deptCode: viewModel.deptCode.value)
+        let viewController = NewNoticeDetailViewController(with: viewModel)
+                                                           
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if didReachedBottom { viewModel.loadNextPage() }
+    }
+}
+
+//  MARK: - Action
+extension NoticesListTableViewController {
+    @objc
+    private func refreshData(sender: UIRefreshControl) {
+        viewModel.loadInitialPage()
+    }
+}
--- SoongsilNotice/ViewControllers/Main/NoticesList/View/NoticesListViewController.swift
+++ SoongsilNotice/ViewControllers/Main/NoticesList/View/NoticesListViewController.swift
@@ -11,29 +11,13 @@ import YDS
 class NoticesListViewController: BaseViewController {
     
     private let viewModel: NoticesListViewModel
-    private let refreshControl = UIRefreshControl()
-    private var didReachedBottom: Bool {
-        self.tableView.contentOffset.y >
-            self.tableView.contentSize.height
-            - self.tableView.bounds.size.height
-            - Dimension.bottomRefreshHeight
-    }
-    private enum Dimension {
-        static let bottomRefreshHeight: CGFloat = 100
-    }
-    
-    private let tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.register(NoticesListItemCell.self, forCellReuseIdentifier: "NoticesListItemCell")
-        tableView.separatorStyle = .none
-        tableView.rowHeight = UITableView.automaticDimension
-        return tableView
-    }()
+    private let tableViewController: NoticesListTableViewController
     
     private let searchButton = YDSTopBarButton(image: YDSIcon.searchLine)
     
     init(with viewModel: NoticesListViewModel) {
         self.viewModel = viewModel
+        self.tableViewController = NoticesListTableViewController(with: viewModel)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -46,7 +30,7 @@ class NoticesListViewController: BaseViewController {
 
         setupViews()
         bindViewModel()
-        setInitialData()
+        tableViewController.setInitialData()
     }
     
     private func setupViews() {
@@ -57,59 +41,34 @@ class NoticesListViewController: BaseViewController {
     
     private func setProperties() {
         self.extendedLayoutIncludesOpaqueBars = true
-        setTitle()
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.refreshControl = refreshControl
-        refreshControl.addTarget(self,
-                                 action: #selector(refreshData(sender:)),
-                                 for: .valueChanged)
         searchButton.addTarget(self,
                                action: #selector(pushSearchViewController(sender:)),
                                for: .touchUpInside)
+        tableViewController.progressBarDelegate = self
     }
     
     private func setViewHierarchy() {
-        self.view.addSubview(tableView)
+        self.embed(tableViewController)
+        self.view.addSubview(tableViewController.view)
         self.navigationItem.setRightBarButtonItems([UIBarButtonItem(customView: searchButton)],
                                                    animated: true)
     }
     
     private func setAutolayout() {
-        tableView.snp.makeConstraints {
+        tableViewController.view.snp.makeConstraints {
             $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             $0.bottom.equalToSuperview()
         }
     }
     
-    private func setTitle() {
-        if self.navigationController?.viewControllers.count ?? 0 > 1 {
-            self.title = viewModel.deptCode.value.getName()
-        }
-    }
-    
     private func bindViewModel() {
-        viewModel.noticesList.bind { [weak self] _ in
+        viewModel.deptCode.bindAndFire { [weak self] value in
             guard let `self` = self else { return }
-            self.tableView.reloadData()
-        }
-        
-        viewModel.nowLoading.bind { [weak self] value in
-            guard let `self` = self else { return }
-            if self.refreshControl.isRefreshing && !value {
-                //  refresh control이 refresh 중인데
-                //  새로 내려온 nowLoading 값이 false인 경우에
-                //  refresch control이 refresh를 중단
-                self.refreshControl.endRefreshing()
+
+            if self.navigationController?.viewControllers.count ?? 0 > 1 {
+                self.title = value.getName()
             }
-            
-            self.hideProgressBar()
         }
-    }
-    
-    private func setInitialData() {
-        viewModel.loadInitialPage()
-        self.showProgressBar()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -118,44 +77,7 @@ class NoticesListViewController: BaseViewController {
 
 }
 
-extension NoticesListViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.noticesList.value.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: NoticesListItemCell =
-            tableView.dequeueReusableCell(withIdentifier: "NoticesListItemCell",
-                                          for: indexPath) as? NoticesListItemCell
-            ?? NoticesListItemCell()
-        let notice = viewModel.noticesList.value[indexPath.row]
-        cell.fillData(with: NoticesListItemViewModel(notice: notice))
-        return cell
-    }
-}
-
-extension NoticesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        let viewModel = NewNoticeDetailViewModel(notice: viewModel.noticesList.value[indexPath.row],
-                                                 deptCode: viewModel.deptCode.value)
-        let viewController = NewNoticeDetailViewController(with: viewModel)
-                                                           
-        self.navigationController?.pushViewController(viewController, animated: true)
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if didReachedBottom { viewModel.loadNextPage() }
-    }
-}
-
 extension NoticesListViewController {
-    @objc
-    private func refreshData(sender: UIRefreshControl) {
-        viewModel.loadInitialPage()
-    }
-    
     @objc
     private func pushSearchViewController(sender: UIControl) {
         let viewController = NoticesSearchListViewController()

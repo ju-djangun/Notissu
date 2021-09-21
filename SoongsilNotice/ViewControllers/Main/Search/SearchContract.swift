eeee73dc824629db89550497d814c6ff22904b13
denny.k <denny.k@kakaocorp.com>
2021-09-21 11:05:48 +0900

    [NSI-123] 검색 화면 스토리보드 제거 완료]

---
+++ SoongsilNotice/ViewControllers/Main/Search/SearchContract.swift
@@ -0,0 +1,24 @@
+//
+//  SearchContract.swift
+//  Notissu
+//
+//  Copyright © 2020 Notissu. All rights reserved.
+//
+
+//import Foundation
+//
+//protocol SearchViewProtocol {
+//    
+//}
+//
+//protocol SearchPresenterProtocol {
+//    func getMajorListItem(at: Int) -> DeptCode
+//    
+//    func getMajorList() -> [DeptCode]
+//    
+//    func getMajorListCount() -> Int
+//}
+//
+//protocol SearchModelProtocol {
+//    func getMajorList() -> [DeptCode]
+//}
